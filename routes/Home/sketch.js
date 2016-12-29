d42451b1084fc922dabd509fa621d6be2820d4cb
yan <yannunes@gmail.com>
2016-12-29 09:33:27 -0300

    Sketch.js working fine

--- routes/Home/sketch.js
+++ routes/Home/sketch.js
@@ -1,14 +1,14 @@
 
-var sketch = function(p){
-  p.setup = function() {
-    p.createCanvas(400,400)
+function sketch(p){
+
+  p.setup = function (){
+    p.createCanvas(1066, 600)
   }
 
-  p.draw = function() {
+  p.draw = function (){
     p.background(51)
   }
+
 }
-
-
 
 export default sketch
