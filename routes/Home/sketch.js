8e15a0303e79bf77658cf7b756d1617b4947b6da
Yan Nunes <yannunes@gmail.com>
2016-12-28 21:16:45 -0300

    Removed with() that was breaking the syntax

--- routes/Home/sketch.js
+++ routes/Home/sketch.js
@@ -1,16 +1,14 @@
-'use strict'
+
 var sketch = function(p){
-  with(p){
-
-    function setup(){
-      createCanvas(400,400)
-    }
-
-    function draw(){
-      background(51)
-    }
+  p.setup = function() {
+    createCanvas(400,400)
   }
 
+  p.draw = function() {
+    background(51)
+  }
 }
+
+
 
 export default sketch
