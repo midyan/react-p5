34dffda669f4345c3208c8c9eb4c404e7304e733
Yan Nunes <yan@campai.com>
2019-07-30 10:40:02 +0200

    better learning

--- routes/Home/p5.js
+++ routes/Home/p5.js
@@ -1,21 +1,21 @@
-var p5 = require('p5')
+const p5 = require('p5');
 
 import React, { PropTypes } from 'react';
-import sketch from './smart_rockets.js'
-import s from './p5.css'
+import sketch from './smart_rockets.js';
+import s from './p5.css';
 
 class P5Wrapper extends React.Component {
   componentDidMount() {
-    this.canvas = new p5(sketch, this.refs.wrapper)
+    this.canvas = new p5(sketch, this.refs.wrapper);
   }
   componentWillReceiveProps(props, newprops) {
-    if( this.canvas.myCustomRedrawAccordingToNewPropsHandler ) {
+    if (this.canvas.myCustomRedrawAccordingToNewPropsHandler) {
       this.canvas.myCustomRedrawAccordingToNewPropsHandler(newprops);
     }
   }
   render() {
-    return  <div className={s.canvasWrapper} ref="wrapper"></div>
+    return <div className={s.canvasWrapper} ref="wrapper" />;
   }
 }
 
-export default P5Wrapper
+export default P5Wrapper;
--- routes/Home/smart_rockets.js
+++ routes/Home/smart_rockets.js
@@ -1,217 +1,215 @@
 function sketch(p) {
+  const populationSize = 300;
+  const lifespan = 5000;
+  let population,
+    target,
+    lifeP,
+    count = 0;
+  const mutationRate = 1;
+  const rx = 130;
+  const ry = 350;
+  const rw = 800;
+  const rh = 30;
 
-  var populationSize = 300
-  var lifespan = 5000
-  var population, target, lifeP, count = 0
-  var mutationRate = 5
-  var rx = 130
-  var ry = 200
-  var rw = 800
-  var rh = 30
-
-  function Random2D(){
-    var angle = Math.random()*p.TWO_PI
-    var x = Math.sin(angle)
-    var y = Math.cos(angle)
-    return p.createVector(x, y)
+  function Random2D() {
+    const angle = Math.random() * p.TWO_PI;
+    const x = Math.sin(angle);
+    const y = Math.cos(angle);
+    return p.createVector(x, y);
   }
 
-  function getRandomElement (array){
-    return array[Math.floor(Math.random()*array.length)]
+  function getRandomElement(array) {
+    return array[Math.floor(Math.random() * array.length)];
   }
 
-  function reset(population){
-    var mCount = 0
-    if(count == lifespan){
-      population.evaluate()
-      population.selection()
-      count = 0
+  function reset(population) {
+    let mCount = 0;
+    if (count == lifespan) {
+      population.evaluate();
+      population.selection();
+      count = 0;
     }
-    for(var i = 0; i<population.rockets.length; i++){
-      if( !population.rockets[i].crashed && !population.rockets[i].completed ) mCount++
+    for (let i = 0; i < population.rockets.length; i++) {
+      if (!population.rockets[i].crashed && !population.rockets[i].completed) mCount++;
     }
-    if(mCount == 0) {
-      population.evaluate()
-      population.selection()
-      count = 0
-    }
-  }
-
-  function Rocket(dna){
-    this.pos = p.createVector(p.width/2, p.height)
-    this.vel = p.createVector(0, -1)
-    this.acc = p.createVector()
-    this.dna = dna? dna : new DNA()
-    this.fitness = 0
-    this.count = 0
-    this.crashed = false
-    this.completed = false
-    this.minDist = p.dist(this.pos.x, this.pos.y, target.x, target.y)
-
-    this.applyForce = function(force){
-      this.acc.add(force)
-    }
-
-    this.update = function(){
-      var d = p.dist(this.pos.x, this.pos.y, target.x, target.y)
-
-      if(!this.crashed&&!this.completed){
-        this.applyForce(this.dna.genes[count])
-        this.vel.add(this.acc)
-        this.pos.add(this.vel)
-        this.acc.mult(0)
-
-        if(this.pos.x<0 || this.pos.x > p.width){
-          this.crashed = true
-        }
-
-        if(this.pos.y<0 || this.pos.y > p.height){
-          this.crashed = true
-        }
-
-        if(this.pos.x > rx && this.pos.x < rx + rw && this.pos.y > ry && this.pos.y < ry + rh){
-          this.crashed = true
-        }
-
-        if( d < this.minDist ) this.minDist = d
-
-      }
-
-
-
-      if(d<15){
-        this.completed = true
-        this.pos = target.copy()
-      }
-
-    }
-
-    this.calcFit = function(){
-      var fitY = p.map(this.pos.y-target.y, 0, p.height, p.height, 0)
-      var fitX = p.map(this.pos.x-target.x, 0, p.height, p.height, 0)
-      var fitCount = 0.5*p.map(count, 0, p.height, p.height, 0)
-      var fitD = p.map(this.minDist, 0, p.height, p.height, 0)
-      this.fitness = !this.crashed? Math.sqrt(fitY*fitY + fitX*fitX + fitCount*fitCount + fitD*fitD) : Math.sqrt(fitY*fitY + fitX*fitX + fitD*fitD)
-      if(this.completed) this.fitness *= 10
-      if(this.crashed) this.fitness *= 0.1
-    }
-
-    this.show = function(){
-      p.push()
-      p.translate(this.pos.x, this.pos.y)
-      p.rotate(this.vel.heading())
-      p.rectMode(p.CENTER)
-      p.rect(0, 0, 45, 7)
-      p.pop()
+    if (mCount == 0) {
+      population.evaluate();
+      population.selection();
+      count = 0;
     }
   }
 
-  function Population(){
-    this.rockets = []
-    this.popsize = populationSize
-    this.avgFit = 0
+  function Rocket(dna) {
+    this.pos = p.createVector(p.width / 2, p.height);
+    this.vel = p.createVector(0, -1);
+    this.acc = p.createVector();
+    this.dna = dna || new DNA();
+    this.fitness = 0;
+    this.count = 0;
+    this.crashed = false;
+    this.completed = false;
+    this.minDist = p.dist(this.pos.x, this.pos.y, target.x, target.y);
 
-    for(var i = 0; i<this.popsize; i++){
-      this.rockets[i] = new Rocket()
+    this.applyForce = function (force) {
+      this.acc.add(force);
+    };
+
+    this.update = function () {
+      const d = p.dist(this.pos.x, this.pos.y, target.x, target.y);
+
+      if (!this.crashed && !this.completed) {
+        this.applyForce(this.dna.genes[count]);
+        this.vel.add(this.acc);
+        this.pos.add(this.vel);
+        this.acc.mult(0);
+
+        if (this.pos.x < 0 || this.pos.x > p.width) {
+          this.crashed = true;
+        }
+
+        if (this.pos.y < 0 || this.pos.y > p.height) {
+          this.crashed = true;
+        }
+
+        if (this.pos.x > rx && this.pos.x < rx + rw && this.pos.y > ry && this.pos.y < ry + rh) {
+          this.crashed = true;
+        }
+
+        if (d < this.minDist) this.minDist = d;
+      }
+
+      if (d < 15) {
+        this.completed = true;
+        this.pos = target.copy();
+      }
+    };
+
+    this.calcFit = function () {
+      const fitY = p.map(Math.abs(this.pos.y - target.y), 0, p.height, p.height, 0);
+      const fitX = p.map(Math.abs(this.pos.x - target.x), 0, p.height, p.height, 0);
+
+      // is after obstacle
+      const multiplier = this.pos.y < ry - 10 ? 100 : 1;
+
+      this.fitness = Math.sqrt(fitY * fitY + fitX * fitX);
+
+      this.fitness *= multiplier;
+      if (this.completed) this.fitness *= 100;
+      if (this.crashed) this.fitness *= 0.01;
+    };
+
+    this.show = function () {
+      p.push();
+      p.translate(this.pos.x, this.pos.y);
+      p.rotate(this.vel.heading());
+      p.rectMode(p.CENTER);
+      p.rect(0, 0, 45, 7);
+      p.pop();
+    };
+  }
+
+  function Population() {
+    this.rockets = [];
+    this.popsize = populationSize;
+    this.avgFit = 0;
+
+    for (let i = 0; i < this.popsize; i++) {
+      this.rockets[i] = new Rocket();
     }
 
-    this.evaluate = function() {
-      var maxfit = 0
-      for(var i = 0; i<this.popsize; i++){
-        this.rockets[i].calcFit()
-        if(this.rockets[i].fitness > maxfit){
-          maxfit = this.rockets[i].fitness
+    this.evaluate = function () {
+      let maxfit = 0;
+      for (var i = 0; i < this.popsize; i++) {
+        this.rockets[i].calcFit();
+        if (this.rockets[i].fitness > maxfit) {
+          maxfit = this.rockets[i].fitness;
         }
       }
 
-      for(var i = 0; i<this.popsize; i++){
-        this.rockets[i].fitness /= maxfit
-        if(this.rockets[i].crashed) this.rockets[i].fitness *= 0.1
+      for (var i = 0; i < this.popsize; i++) {
+        this.rockets[i].fitness /= maxfit;
       }
 
-      this.matingpool = []
-      var sum = 0
-      for(var i = 0; i<this.popsize; i++){
-        sum += this.rockets[i].fitness
-        var n = this.rockets[i].fitness*100
+      this.matingpool = [];
+      let sum = 0;
+      for (var i = 0; i < this.popsize; i++) {
+        sum += this.rockets[i].fitness;
+        const n = this.rockets[i].fitness * 10000;
         // console.log("Foguete "+i+": "+n)
-        for(var j = 0; j<n; j++){
-          this.matingpool.push(this.rockets[i])
+        for (let j = 0; j < n; j++) {
+          this.matingpool.push(this.rockets[i]);
         }
       }
-      this.avgFit = sum/this.popsize
-      console.log(this.avgFit)
+      this.avgFit = sum / this.popsize;
+      console.log('average fitness', this.avgFit);
+    };
 
-
-    }
-
-    this.selection = function(){
-      var newRockets = []
-      for(var i = 0; i<this.rockets.length; i++){
-        var parentA = getRandomElement(this.matingpool).dna
-        var parentB = getRandomElement(this.matingpool).dna
-        var child = parentA.crossover(parentB)
-        newRockets[i] = new Rocket(child)
+    this.selection = function () {
+      const newRockets = [];
+      for (let i = 0; i < this.rockets.length; i++) {
+        const parentA = getRandomElement(this.matingpool).dna;
+        const parentB = getRandomElement(this.matingpool).dna;
+        const child = parentA.crossover(parentB);
+        newRockets[i] = new Rocket(child);
       }
-      this.rockets = newRockets
-    }
+      this.rockets = newRockets;
+    };
 
-    this.run = function(){
-      for(var i = 0; i<this.popsize; i++){
-        this.rockets[i].update()
-        this.rockets[i].show()
+    this.run = function () {
+      for (let i = 0; i < this.popsize; i++) {
+        this.rockets[i].update();
+        this.rockets[i].show();
       }
-    }
+    };
   }
 
-  function DNA(genes){
-    if(genes){
-      this.genes = genes
-    }else{
-      this.genes = []
-      for(var i = 0; i<lifespan; i++){
-        this.genes[i] = Random2D()
-        this.genes[i].setMag(0.1)
+  function DNA(genes) {
+    if (genes) {
+      this.genes = genes;
+    } else {
+      this.genes = [];
+      for (let i = 0; i < lifespan; i++) {
+        this.genes[i] = Random2D();
+        this.genes[i].setMag(0.1);
       }
     }
 
-    this.crossover = function(partner){
-      var newgenes = []
-      var mid = Math.floor( Math.random()*this.genes.length )
-      var geneCount = 0
-      for(var i = 0; i<this.genes.length; i++){
-        if(i>mid){
-          newgenes[i] = this.genes[i]
-        }else{
-          newgenes[i] = partner.genes[i]
+    this.crossover = function (partner) {
+      const newgenes = [];
+      const mid = Math.floor(Math.random() * this.genes.length);
+      let geneCount = 0;
+      for (let i = 0; i < this.genes.length; i++) {
+        if (i > mid) {
+          newgenes[i] = this.genes[i];
+        } else {
+          newgenes[i] = partner.genes[i];
         }
-        var mutateDice = Math.floor(Math.random()*100)
+        const mutateDice = Math.floor(Math.random() * 100);
 
-        if(mutateDice < mutationRate){
-          geneCount++
-          newgenes[i] = Random2D()
+        if (mutateDice < mutationRate) {
+          geneCount++;
+          newgenes[i] = Random2D();
         }
       }
-      return new DNA(newgenes)
-    }
+      return new DNA(newgenes);
+    };
   }
 
-  p.setup = function (){
-    p.createCanvas(1066, 500)
-    target = p.createVector(p.width/2, 50)
-    population = new Population()
+  p.setup = function () {
+    p.createCanvas(1066, 500);
+    target = p.createVector(p.width / 2, 50);
+    population = new Population();
     // p.rect(rx, ry, rw, rh)
-  }
+  };
 
-  p.draw = function (){
-    p.background(51)
-    population.run()
-    p.ellipse(target.x, target.y, 16, 16)
-    p.rect(rx, ry, rw, rh)
-    count++
-    reset(population)
-  }
-
+  p.draw = function () {
+    p.background(51);
+    population.run();
+    p.ellipse(target.x, target.y, 16, 16);
+    p.rect(rx, ry, rw, rh);
+    count++;
+    reset(population);
+  };
 }
 
-export default sketch
+export default sketch;
